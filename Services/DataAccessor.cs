b593c8c97da2a153938d21b39908fec2c33fb551
Chuck Allen <ChuckAllen@QuickenLoans.com>
2020-09-23 15:42:16 -0400

    Data access layer implemented

---
+++ Services/DataAccessor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper.Contrib.Extensions;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindAPI.Services
+{
+    public class DataAccessor : IDataAccessor
+    {
+        IDbConnection db;
+
+        public DataAccessor(IConfiguration config)
+        {
+            db = new SqlConnection(config.GetConnectionString("DbServer"));
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NorthwindAPI.Services;
 
 namespace NorthwindAPI
 {
@@ -26,6 +27,7 @@ namespace NorthwindAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<IDataAccessor, DataAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
