36641d3b08138154f808730d3a372c515aacb60b
David Heizer <DavidHeizer@QuickenLoans.com>
2020-09-23 16:28:30 -0400

    Tables mapped to Model Classes

---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+namespace NorthwindAPI.Models
+{
+    [Table("Customers")]
+    public class Customer
+    {
+        [Key]
+        public long CustomerID { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+
+    }
+}
